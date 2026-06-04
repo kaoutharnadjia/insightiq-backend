36f392b73003cacfe78f50a88477d7a5111341ba
unknown <barkisaberziad@gmail.com>
2026-06-05 00:13:42 +0200

    Fix sales

--- services/chatService.js
+++ services/chatService.js
@@ -4,7 +4,7 @@ const ERPClient = require("../erp-client/erpClient");
 const normalizer = require("../normalizers/normalizer");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 const chatService = {
   /**
